febac4b57572bcdfe4a2eccabd139e1451655f82
Jaimon Varghese <jaimonvarghese1224@gmail.com>
2024-07-17 22:29:31 +0530

    ASSets&Fonts folders create and definge in pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'core/configs/theme/app_theme.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,11 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Spotify',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: AppTheme.lightTheme,
       home: Container(),
     );
   }
